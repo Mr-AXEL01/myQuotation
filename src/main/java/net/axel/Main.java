462254b3fc725b194a544575305bd116b1cd9bf6
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-09-17 03:40:02 +0100

    check DB connection.

--- src/main/java/net/axel/Main.java
+++ src/main/java/net/axel/Main.java
@@ -1,7 +1,24 @@
 package net.axel;
 
+import net.axel.config.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+//        try {
+//            Connection connection = DatabaseConnection.getInstance().getConnection();
+//            if (connection != null && !connection.isClosed()) {
+//                System.out.println("Database connection successful!");
+//            } else {
+//                System.out.println("Failed to connect to the database.");
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
     }
+
 }
