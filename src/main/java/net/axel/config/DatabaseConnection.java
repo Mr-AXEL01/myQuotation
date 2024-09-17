65b260640ddaad72c2a4b2c9fd4d85a76cbcac45
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-09-17 18:43:04 +0100

    update the class name of env to call the .env variable

--- src/main/java/net/axel/config/DatabaseConnection.java
+++ src/main/java/net/axel/config/DatabaseConnection.java
@@ -1,6 +1,6 @@
 package net.axel.config;
 
-import net.axel.utils.env;
+import net.axel.utils.Env;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -33,9 +33,9 @@ public class DatabaseConnection {
     }
 
     private void init() throws SQLException {
-        final String URL = env.get("URL");
-        final String USER = env.get("USER");
-        final String PASSWORD = env.get("PASSWORD");
+        final String URL = Env.get("URL");
+        final String USER = Env.get("USER");
+        final String PASSWORD = Env.get("PASSWORD");
         try {
             connection = DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (SQLException e) {
