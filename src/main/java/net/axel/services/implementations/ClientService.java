70617ca72baf136b2e84dc8b6e9d73008cc3898a
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-09-18 04:48:25 +0100

    implement the add client method

--- src/main/java/net/axel/services/implementations/ClientService.java
+++ src/main/java/net/axel/services/implementations/ClientService.java
@@ -1,4 +1,38 @@
 package net.axel.services.implementations;
 
-public class ClientService {
+import net.axel.models.dto.ClientDto;
+import net.axel.models.entities.Client;
+import net.axel.repositories.implementations.ClientRepository;
+import net.axel.repositories.interfaces.IClientRepository;
+import net.axel.services.interfaces.IClientService;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class ClientService implements IClientService {
+    private final IClientRepository clientRepository;
+
+    public ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    @Override
+    public void addClient(ClientDto clientDto) {
+        if (clientExists(clientDto.name())) {
+            throw new IllegalArgumentException("Client with name " + clientDto.name() + " already exists.");
+        }
+        Client client = new Client(
+                UUID.randomUUID(),
+                clientDto.name(),
+                clientDto.address(),
+                clientDto.phone(),
+                clientDto.isProfessional()
+        );
+        clientRepository.addClient(client);
+    }
+
+    private boolean clientExists(String name) {
+        return clientRepository.findByName(name).isPresent();
+    }
 }
