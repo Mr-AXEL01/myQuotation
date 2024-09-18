766ca1d158b8ec82188b5f1ecafe349bfdb17830
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-09-18 04:47:47 +0100

    added : set the methods signatures.

--- src/main/java/net/axel/services/interfaces/IClientService.java
+++ src/main/java/net/axel/services/interfaces/IClientService.java
@@ -1,5 +1,21 @@
 package net.axel.services.interfaces;
 
+import net.axel.models.dto.ClientDto;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IClientService {
+
+    void addClient(ClientDto clientDto);
+
+    Optional<ClientDto> findClientByName(String name);
+
+    List<ClientDto> findAllClients();
+
+    void updateClient(String oldName, ClientDto updatedClient);
+
+    void deleteClient(String name);
+}
 
 }
