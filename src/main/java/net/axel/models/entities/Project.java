298b3a1813920621df4b6195568e59baf4fa68eb
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-09-19 15:56:31 +0100

    added : create default constructor and a used constructor + getter and setter for the project.

--- src/main/java/net/axel/models/entities/Project.java
+++ src/main/java/net/axel/models/entities/Project.java
@@ -14,5 +14,72 @@ public class Project {
     private ProjectStatus projectStatus;
     private Client client;
 
-    
+    public Project() {
+    }
+
+    public Project(UUID id, String name, Double surface, Double profitMargin, Double totalCost, ProjectStatus projectStatus, Client client) {
+        this.id = id;
+        this.name = name;
+        this.surface = surface;
+        this.profitMargin = profitMargin;
+        this.totalCost = totalCost;
+        this.projectStatus = projectStatus;
+        this.client = client;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getSurface() {
+        return surface;
+    }
+
+    public void setSurface(Double surface) {
+        this.surface = surface;
+    }
+
+    public Double getProfitMargin() {
+        return profitMargin;
+    }
+
+    public void setProfitMargin(Double profitMargin) {
+        this.profitMargin = profitMargin;
+    }
+
+    public Double getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(Double totalCost) {
+        this.totalCost = totalCost;
+    }
+
+    public ProjectStatus getProjectStatus() {
+        return projectStatus;
+    }
+
+    public void setProjectStatus(ProjectStatus projectStatus) {
+        this.projectStatus = projectStatus;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
 }
