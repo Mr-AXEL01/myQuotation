f9b6a43d540f896d63a564eb9f57a507b6e099c4
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-09-24 12:16:11 +0100

    added : set the component repository implementation for the case the can use it for both labor and material.

--- src/main/java/net/axel/repositories/implementations/ComponentRepository.java
+++ src/main/java/net/axel/repositories/implementations/ComponentRepository.java
@@ -2,5 +2,5 @@ package net.axel.repositories.implementations;
 
 import net.axel.repositories.interfaces.IComponentRepository;
 
-public class ComponentRepository implements IComponentRepository {
+public class ComponentRepository{
 }
