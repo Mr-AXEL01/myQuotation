6180e25a528bd99c68aed45f84ba1777649c451e
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-09-24 12:06:35 +0100

    added : instance project object with  all the project attributs.

--- src/main/java/net/axel/services/implementations/ProjectService.java
+++ src/main/java/net/axel/services/implementations/ProjectService.java
@@ -3,7 +3,6 @@ package net.axel.services.implementations;
 import net.axel.models.dto.ProjectDto;
 import net.axel.models.entities.Client;
 import net.axel.models.entities.Project;
-import net.axel.models.enums.ProjectStatus;
 import net.axel.repositories.implementations.ProjectRepository;
 import net.axel.repositories.interfaces.IProjectRepository;
 import net.axel.services.interfaces.IClientService;
@@ -22,13 +21,14 @@ public class ProjectService implements IProjectService {
 
     public Project addProject(ProjectDto dto) {
         Client client = clientService.findClientById(dto.clientId());
-        final ProjectStatus projectStatus = ProjectStatus.IN_PROGRESS;
 
         Project project = new Project(
                 UUID.randomUUID(),
                 dto.name(),
                 dto.surface(),
-                projectStatus,
+                dto.profitMargin(),
+                dto.totalCost(),
+                dto.projectStatus(),
                 client
         );
 
